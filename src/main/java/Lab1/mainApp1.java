ff0226556b0d2fae3b0cc6fd1c5ec860a4a8a504
hhliag <nathanhhli@gmail.com>
2026-09-20 21:22:39 +0800

    Add 2 print lines

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -2,6 +2,8 @@ package Lab1;
 
 public class mainApp1 {
     public static void main(String[] args) {
+        System.out.println("COMP3111 Lab1 Report");
+        System.out.println("====================");
         System.out.println("2^10 = " + myLibrary.power(2, 10));
         System.out.println("5! = " + myLibrary.factorial(5));
     }
